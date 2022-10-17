1ebf0b35950f56d5561a1e6346c6e6053f60b464
ezyxels <ezyxels@gmail.com>
2022-10-17 13:57:17 +0200

    gallery swipe down close

--- src/components/zajezd/Gallery.tsx
+++ src/components/zajezd/Gallery.tsx
@@ -28,8 +28,9 @@ export default function Gallery({ images }: Props) {
 
 
   handlers = useSwipeable({
-    onSwipedRight: () => { (imageIndex !== undefined && imageIndex > 0) && setImageIndex(imageIndex - 1) },
     onSwipedLeft: () => { (imageIndex !== undefined && imageIndex < (images.length - 1)) && setImageIndex(imageIndex + 1) },
+    onSwipedRight: () => { (imageIndex !== undefined && imageIndex > 0) && setImageIndex(imageIndex - 1) },
+    onSwipedUp: () => setImageIndex(undefined),
     onSwipedDown: () => setImageIndex(undefined)
   })
 
