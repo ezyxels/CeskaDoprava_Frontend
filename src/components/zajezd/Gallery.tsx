6bc9d147fe1fb701db900811be4b26fd46cf0920
ezyxels <ezyxels@gmail.com>
2022-11-07 15:57:42 +0100

    snad vole

--- src/components/zajezd/Gallery.tsx
+++ src/components/zajezd/Gallery.tsx
@@ -42,14 +42,14 @@ export default function Gallery({ images }: Props) {
 
       {/* Opened gallery */}
       {imageIndex !== undefined &&
-        <div className="fixed z-[130] inset-0 bg-black">
+        <div className="fixed z-[130] inset-0 bg-black/95 backdrop-blur-sm">
           <div className="w-full relative h-3/4 flex flex-row items-center justify-center md:gap-x-20 px-5">
             {/* Gallery arrow left */}
             <span
               onClick={() => imageIndex !== 0 && setImageIndex(imageIndex - 1)}
               className={`absolute top-0 left-0 w-1/4 h-full md:w-10 md:h-10
                 md:static flex text-3xl cursor-pointer md:hover:scale-125 duration-150
-                rounded-md md:border-2 border-gray-500 text-gray-500 z-[150]
+                rounded-md md:border-2 border-white text-white z-[150]
                 md:items-center justify-center
                 `}
 
@@ -62,7 +62,7 @@ export default function Gallery({ images }: Props) {
             <span
               onClick={() => setImageIndex(undefined)}
               className="top-5 right-5 absolute text-3xl cursor-pointer hover:scale-125 duration-150
-              rounded-md border-2 border-gray-500 text-gray-500 z-[160]"
+              rounded-md border-2 border-white text-white z-[160]"
             >
               <HiX />
             </span>
@@ -86,7 +86,7 @@ export default function Gallery({ images }: Props) {
               onClick={() => imageIndex !== (images.length - 1) && setImageIndex(imageIndex + 1)}
               className={`absolute top-0 right-0 w-1/4 h-full md:w-10 md:h-10
                 md:static flex text-3xl cursor-pointer md:hover:scale-125 duration-150
-                rounded-md md:border-2 border-gray-500 text-gray-500 z-[150]
+                rounded-md md:border-2 border-white text-white z-[150]
                 md:items-center justify-center
                 `}
             >
